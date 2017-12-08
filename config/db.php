fa20a371334fdabf07b7a6d28f16bc959a56b4dd
Gaute Rønningen <rnngau@gmail.com>
2017-12-08 12:49:08 +0100

    Changed configuration to fit demo server

--- config/db.php
+++ config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=bwnraids',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+    'dsn' => 'mysql:host=localhost;dbname=gaute_bwnraids',
+    'username' => 'gaute_bwnraids',
+    'password' => 'gNrIYAshM',
+    'charset' => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
