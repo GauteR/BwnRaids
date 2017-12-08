a02e4596bec6d09d5f2d594f7bbb8a6df9c9c558
Gaute Rønningen <rnngau@gmail.com>
2017-12-08 14:27:52 +0100

    Added Users CRUD

---
+++ views/users/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Users */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="users-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'user_fk_rank')->textInput() ?>
+
+    <?= $form->field($model, 'user_email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'user_pass')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'user_discord')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
